7585a4262bb7d7fc38d6bd22aadb9747d78463b8
VieleSchritte <ohheavenineed@gmail.com>
2021-03-18 16:08:59 +0300

    SOLVED THIS F***ING DB PROBLEM

--- jsjobs_app/__init__.py
+++ jsjobs_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
-#from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
@@ -10,10 +10,11 @@ import os
 # Initiating app from Flask and configuring it with Config of file config.py
 app = Flask('js_jobs', template_folder='templates/', static_folder='static/')
 app.config.from_object(Config)
-Session(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# Setting up database
+"""# Setting up database
 engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+db = scoped_session(sessionmaker(bind=engine))"""
 
-from jsjobs_app import urls, models
+from jsjobs_app import views, models
--- run.py
+++ run.py
@@ -1,1 +1,6 @@
-from jsjobs_app import app
+from jsjobs_app import app, db
+from jsjobs_app.models import Company
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Company': Company}
